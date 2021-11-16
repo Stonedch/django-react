32464016b167cf2399f0ee425286fed3a535770f
Maxim Mukhin <stch.max.muhin@gmail.com>
2021-11-16 12:01:06 +0300

    feat: add crud api backend

--- djangoreactproject/urls.py
+++ djangoreactproject/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+from customers import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('api/customers/', views.customers_list),
+    url('api/customers/(?P<pk>[0-9]+)', views.customers_detail),
 ]
